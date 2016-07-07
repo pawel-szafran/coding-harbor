c0ffc9a75b4b191069379445dda8f7476ee4bea0
Pawel Szafran <pawel.szafran@gmail.com>
2016-07-06 21:19:04 -0400

    Clean up

--- chess/board/board.go
+++ chess/board/board.go
@@ -25,50 +25,20 @@ const (
 	capturedSquare
 )
 
-const (
-	bitsPerSquare  = 2
-	squaresPerByte = 8 / bitsPerSquare
-	twoLowBits     = 0x3
-)
-
 func New(size Size) *Board {
-	squaresCount := size.Rows * size.Cols
-	bytesCount := squaresCount / squaresPerByte
-	if squaresCount%squaresPerByte != 0 {
-		bytesCount++
-	}
 	return &Board{
 		size:    size,
-		squares: make([]byte, bytesCount),
+		squares: newSquares(size),
 		CurPos:  Pos{0, -1},
 	}
 }
 
 func (b *Board) Copy() *Board {
-	squares := make([]byte, len(b.squares))
-	copy(squares, b.squares)
-	return &Board{b.size, squares, b.CurPos}
+	return &Board{b.size, copySquares(b.squares), b.CurPos}
 }
 
 func (b *Board) Rows() int8 { return b.size.Rows }
 func (b *Board) Cols() int8 { return b.size.Cols }
-
-func (b *Board) checkSquare(p Pos, s square) bool {
-	idx, shift := b.findSquare(p)
-	return (b.squares[idx]>>shift)&twoLowBits == byte(s)
-}
-
-func (b *Board) setSquare(p Pos, s square) {
-	idx, shift := b.findSquare(p)
-	b.squares[idx] |= byte(s) << shift
-}
-
-func (b *Board) findSquare(p Pos) (idx int8, shift uint8) {
-	squareIdx := p.Row*b.size.Cols + p.Col
-	idx = squareIdx / squaresPerByte
-	shift = uint8((squareIdx % squaresPerByte) * bitsPerSquare)
-	return
-}
 
 func (b *Board) MoveToNextSafeSquare() bool {
 	for b.moveToNextSquare() {
@@ -111,4 +81,42 @@ func (b *Board) PlacePiece(piece Piece) bool {
 func (b *Board) contains(p Pos) bool {
 	return p.Row >= 0 && p.Row < b.Rows() &&
 		p.Col >= 0 && p.Col < b.Cols()
+}
+
+const (
+	bitsPerSquare  = 2
+	squaresPerByte = 8 / bitsPerSquare
+	squareMask     = 0x3
+)
+
+func newSquares(size Size) []byte {
+	squaresCount := size.Rows * size.Cols
+	bytesCount := squaresCount / squaresPerByte
+	if squaresCount%squaresPerByte != 0 {
+		bytesCount++
+	}
+	return make([]byte, bytesCount)
+}
+
+func copySquares(sqs []byte) []byte {
+	sqsCopy := make([]byte, len(sqs))
+	copy(sqsCopy, sqs)
+	return sqsCopy
+}
+
+func (b *Board) checkSquare(p Pos, s square) bool {
+	idx, shift := b.findSquare(p)
+	return (b.squares[idx]>>shift)&squareMask == byte(s)
+}
+
+func (b *Board) setSquare(p Pos, s square) {
+	idx, shift := b.findSquare(p)
+	b.squares[idx] |= byte(s) << shift
+}
+
+func (b *Board) findSquare(p Pos) (idx int8, shift uint8) {
+	squareIdx := p.Row*b.size.Cols + p.Col
+	idx = squareIdx / squaresPerByte
+	shift = uint8((squareIdx % squaresPerByte) * bitsPerSquare)
+	return
 }
