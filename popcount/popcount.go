36a8fd6f57339c22f08b209558133addf8af8b45
Pawel Szafran <pawel.szafran@gmail.com>
2016-08-09 23:00:19 -0400

    [popcount] Add CountSlice

--- popcount/popcount.go
+++ popcount/popcount.go
@@ -1,11 +1,20 @@
 package popcount
 
+type CountFunc func(uint32) uint32
+
 func CountNaive(v uint32) (c uint32) {
 	for i := 0; i < 32; i++ {
 		if v&0x1 > 0 {
 			c++
 		}
 		v >>= 1
+	}
+	return
+}
+
+func CountSlice(values []uint32, count CountFunc) (c uint32) {
+	for _, v := range values {
+		c += count(v)
 	}
 	return
 }
--- popcount/popcount_test.go
+++ popcount/popcount_test.go
@@ -12,8 +12,18 @@ func TestCountNaive(t *testing.T) {
 	}
 	for _, tt := range tests {
 		c := CountNaive(tt.x)
-		if c != tt.c {
-			t.Errorf("Want %d, got %d", tt.c, c)
-		}
+		assertCount(t, c, tt.c)
+	}
+}
+
+func TestCountSlice(t *testing.T) {
+	fakeCount := func(x uint32) uint32 { return x }
+	c := CountSlice([]uint32{1, 3, 5, 8, 13}, fakeCount)
+	assertCount(t, c, 30)
+}
+
+func assertCount(t *testing.T, got, want uint32) {
+	if got != want {
+		t.Errorf("Want %d, got %d", want, got)
 	}
 }
